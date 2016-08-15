0d56fdfcdb455b5f18458ca2c717cb4a78035786
Lucy Fox <lucyreneefox@gmail.com>
2016-08-15 16:38:57 -0600

    Create and update working as expected for api ideas controller and all test passing

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -5,4 +5,26 @@ class Api::V1::IdeasController < ApiBaseController
     ideas = { ideas: Idea.all}
     respond_with ideas
   end
+
+  def create
+    idea = Idea.new(idea_params)
+    if idea.save
+      respond_with({ idea: idea }, status: 201, location: api_v1_ideas_path)
+    else
+      respond_with({ errors: idea.errors }, status: 422, location: api_v1_ideas_path)
+    end
+  end
+
+  def update
+    idea = Idea.find(params[:id])
+    if idea.update(idea_params)
+      respond_with({ idea: idea}, status: 200, location: api_v1_ideas_path)
+    else
+      respond_with({errors: idea.errors}, status: 422, location: api_v1_ideas_path)
+    end
+  end
+
+  def idea_params
+    params.require(:idea).permit(:title, :body, :quality)
+  end
 end
--- spec/requests/api/v1/ideas_controller_spec.rb
+++ spec/requests/api/v1/ideas_controller_spec.rb
@@ -12,4 +12,33 @@ describe "Ideas Controller" do
     expect(json["ideas"].second["title"]).to eq(idea_2.title)
     expect(json["ideas"].second["body"]).to eq(idea_2.body)
   end
+
+  it "creates a new idea" do
+    params = {idea: {title: "New Idea", body: "This is the body"}}
+
+    post '/api/v1/ideas', params: params
+
+    expect(response).to be_success
+
+    idea = Idea.first
+
+    expect(idea.title).to eq("New Idea")
+    expect(idea.body).to eq("This is the body")
+  end
+
+  it "updates an existing idea" do
+    idea = create(:idea, title: "Old title", body: "This is the idea body", quality: 0)
+    id = idea.id
+    put "/api/v1/ideas/#{id}", params: {idea: {title: "New Title", quality: 1}}
+
+    expect(response).to be_success
+    updated_idea = Idea.find(id)
+
+    expect(updated_idea.title).to eq("New Title")
+    expect(updated_idea.quality).to eq(1)
+  end
+
+  it "deletes an idea from the database" do
+
+  end
 end
