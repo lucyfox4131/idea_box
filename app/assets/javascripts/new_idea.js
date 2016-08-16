63d032806a64189a936567731472f80b221c094e
Lucy Fox <lucyreneefox@gmail.com>
2016-08-16 13:08:22 -0600

    Add upvote downvote icons to table

--- app/assets/javascripts/new_idea.js
+++ app/assets/javascripts/new_idea.js
@@ -20,7 +20,8 @@ $(document).ready(function(){
   };
 
   function addNewIdea(idea){
-    $("#table-headings").after("<tr id='data"+ idea.id+"'><td>"+ idea.title +"</td><td>"+ truncateBody(idea.body) +"</td><td>"+ qualityOutput(idea.quality) +"</td><td><a class='delete' id='" +  idea.id + "'><i class='fa fa-trash-o' aria-hidden='true'></i></a></td></tr>")
+    $("#table-headings").after(
+      "<tr id='data"+ idea.id+"'><td>"+ idea.title +"</td><td>"+ truncateBody(idea.body) +"</td><td>"+ qualityOutput(idea.quality) +"</td> <td><a class='upvote' id='" +  idea.id + "'><i class='fa fa-thumbs-o-up' aria-hidden='true'></i></a></td> <td><a class='downvote' id='" +  idea.id + "'><i class='fa fa-thumbs-o-down' aria-hidden='true'></i></a></td> <td><a class='delete' id='" +  idea.id + "'><i class='fa fa-trash-o' aria-hidden='true'></i></a></td></tr>")
   };
 
 
