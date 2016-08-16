743c040d57d82921c74296d35fa740f030902d08
Lucy Fox <lucyreneefox@gmail.com>
2016-08-16 14:01:39 -0600

    Testing for truncated body happy path for truncate Body function and for quality output all outcomes

---
+++ spec/javascripts/new_idea_spec.js
@@ -0,0 +1,34 @@
+//= require new_idea
+
+describe('Quality Output', function(){
+  it('returns Swill for 0', function(){
+    var quality = 0
+    var result = "Swill"
+
+    expect(qualityOutput(quality)).to.equal(result)
+  });
+
+  it('returns Plausible for 1', function(){
+    var quality = 1
+    var result = "Plausible"
+
+    expect(qualityOutput(quality)).to.equal(result)
+  });
+
+  it('returns Genius for 2', function(){
+    var quality = 2
+    var result = "Genius"
+
+    expect(qualityOutput(quality)).to.equal(result)
+  });
+});
+
+describe('truncateBody', function(){
+  it("successfully shortens long body", function(){
+    var body = "Bacon ipsum dolor amet spare ribs andouille biltong, strip steak rump flank corned beef salami. Ham andouille hamburger, salami ball tip bacon flank. Landjaeger tenderloin frankfurter hamburger. Flank tongue alcatra pork loin ribeye."
+    var result = "Bacon ipsum dolor amet spare ribs andouille biltong, strip steak rump flank corned beef salami. Ham"
+
+    expect(truncateBody(body)).to.equal(result);
+    expect(truncateBody(body).length).to.be.below(100);
+  });
+});
